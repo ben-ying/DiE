ae067ba53b064eb5b119cb6af5e5e30c8cdf7384
ben <benying1988@gmail.com>
2019-06-17 11:48:29 +0800

    add data binding

--- app/src/main/java/com/yjh/die/MainActivity.java
+++ app/src/main/java/com/yjh/die/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 import com.google.android.material.navigation.NavigationView;
+import com.yjh.die.databinding.ActivityMainBinding;
+
+import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +23,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil
+                .setContentView(this, R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
